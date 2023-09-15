0b83e38664ddaad0af3f4a1cd4bc3ebab8f448d7
lgm3405@naver.com <lgm3405@naver.com>
2023-09-15 11:25:34 +0900

    test : for git merge

--- RocketLeague/Assets/LGM_Project/Scripts/GameManager.cs
+++ RocketLeague/Assets/LGM_Project/Scripts/GameManager.cs
@@ -58,30 +58,4 @@ public class GameManager : MonoBehaviourPunCallbacks /*IPunObservable*/
             PhotonNetwork.Instantiate(orangeCar.name, orangeCarSpawner[0].position, orangeCarSpawner[0].rotation, 0);
         }
     }
-
-    // 주기적으로 자동 실행되는, 동기화 메서드
-    //public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    //{
-    //    // 로컬 오브젝트라면 쓰기 부분이 실행됨
-    //    if (stream.IsWriting)
-    //    {
-    //        // 네트워크를 통해 score 값을 보내기
-    //        stream.SendNext(teamCount[0]);
-    //        stream.SendNext(teamCount[1]);
-    //    }
-    //    else
-    //    {
-    //        // 리모트 오브젝트라면 읽기 부분이 실행됨
-
-    //        // 네트워크를 통해 score 값 받기
-    //        teamCount[0] = (int)stream.ReceiveNext();
-    //        teamCount[1] = (int)stream.ReceiveNext();
-    //        // 동기화하여 받은 점수를 UI로 표시
-
-    //        teamCountText[0].text = string.Format("{0} / {1}", teamCount[0], maxTeamCount);   // 증가시킨 값을 출력
-    //        teamCountText[1].text = string.Format("{0} / {1}", teamCount[1], maxTeamCount);
-
-    //        AfterSerializeView();
-    //    }
-    //}
 }
--- RocketLeague/Assets/LGM_Project/Scripts/LobbyManager.cs
+++ RocketLeague/Assets/LGM_Project/Scripts/LobbyManager.cs
@@ -12,7 +12,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     void Awake()
     {
-        gameVersion = "0.1.0";   // 게임 버전 값
+        gameVersion = "0.2.0";   // 게임 버전 값
     }
 
        // 게임 실행과 동시에 마스터 서버 접속 시도
@@ -75,7 +75,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
            // 접속 상태 표시
         connectionInfoText.text = "Nothing to empty room, Create new room ...";
            // 최대 6명 수용 가능한 빈 방 생성
-        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 6 });
     }      // OnJoinRandomFailed()
 
 
